a7a90b45e5c679ac47d41b2ae32fdbe3e613714f
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-05-28 01:06:30 +0530

    fix cors error

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://invihelp.vercel.ap"],
+    origin: ["http://localhost:3000", "https://invihelp.vercel.app"],
     credentials: true,
   })
 );
